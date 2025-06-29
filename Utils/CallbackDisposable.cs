fa25e035c07c063a6835cbae293fcbd065305132
Open-KFC <1811593346@qq.com>
2025-06-29 17:01:29 +0800

    feat(SetupJsonManager): 实现一个 json 配置文件托管类
    todo: remove PCL.Core.Helper.Configure.*

---
+++ Utils/CallbackDisposable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PCL.Core.Utils;
+
+/// <summary>
+/// 用于使用 Using 的语法糖
+/// </summary>
+public class CallbackDisposable(Action callback) : IDisposable
+{
+    public void Dispose() => callback.Invoke();
+}

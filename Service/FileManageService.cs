7c5a53d62420d3c5bbb381ba6eb1c733e63b1d59
Open-KFC <1811593346@qq.com>
2025-07-01 12:16:46 +0800

    feat(FileManage): 新建文件夹

---
+++ Service/FileManageService.cs
@@ -0,0 +1,20 @@
+﻿using PCL.Core.LifecycleManagement;
+
+namespace PCL.Core.Service;
+
+[LifecycleService(LifecycleState.Loading, Priority = 10000)]
+public class FileManageService : ILifecycleService
+{
+    public string Identifier => "file-manage";
+    public string Name => "文件资源管理";
+    public bool SupportAsyncStart => false;
+    public void Start()
+    {
+        
+    }
+
+    public void Stop()
+    {
+        
+    }
+}

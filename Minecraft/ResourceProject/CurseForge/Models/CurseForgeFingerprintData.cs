995802719c2c59b75a78b32aabb7da4fb449c3b3
薄奚梦灵 <boximengling@outlook.com>
2025-08-14 10:20:23 +0800

    fix(resource): 数据类型有误

--- Minecraft/ResourceProject/CurseForge/Models/CurseForgeFingerprintData.cs
+++ Minecraft/ResourceProject/CurseForge/Models/CurseForgeFingerprintData.cs
@@ -6,12 +6,12 @@ namespace PCL.Core.Minecraft.ResourceProject.CurseForge.Models;
 public record CurseForgeFingerprintData
 {
     [JsonPropertyName("isCacheBuilt")] public bool IsCacheBuilt;
-    [JsonPropertyName("exactMatches")] public required CurseForgeMatches ExactMatches;
+    [JsonPropertyName("exactMatches")] public required List<CurseForgeMatches> ExactMatches;
 
     [JsonPropertyName("exactFingerprints")]
     public required List<int> ExactFingerprints;
 
-    [JsonPropertyName("partialMatches")] public required CurseForgeMatches PartialMatches;
+    [JsonPropertyName("partialMatches")] public required List<CurseForgeMatches?> PartialMatches;
     
     [JsonPropertyName("partialMatchFingerprints")]
     public required CurseForgeMatchFingerprint PartialMatchFingerprints;

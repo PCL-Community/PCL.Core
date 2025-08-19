ce4c5ebc2353096de649d96a4d4907dc213dfcb3
tangge233 <tangge233@qq.com>
2025-08-19 18:01:28 +0800

    refactor(http): 重构 builder 实现

---
+++ Net/HttpResponseHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+
+namespace PCL.Core.Net;
+
+public class HttpResponseHandler(HttpResponseMessage responseMessage) : IDisposable
+{
+    public bool IsSuccess { get; } = responseMessage.IsSuccessStatusCode;
+
+    public void Dispose()
+    {
+        responseMessage.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}

21f45728c1ce6e60d06e60454338243ff8297f4b
Hill23333 <a18035135032@163.com>
2025-09-01 23:31:29 +0800

    feat: AsyncCountResetEvent

---
+++ Utils/Threading/AsyncCountResetEvent.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PCL.Core.Utils.Threading;
+
+// 使用 ChatGPT 生成的代码
+
+/// <summary>
+/// 一个带配额的 AsyncAutoResetEvent。
+/// </summary>
+public class AsyncCountResetEvent
+{
+    private readonly Queue<TaskCompletionSource<bool>> _waiters = new();
+    private int _permits; // 当前剩余的"配额"
+    private readonly object _lock = new();
+    
+    public Task WaitAsync()
+    {
+        lock (_lock)
+        {
+            if (_permits > 0)
+            {
+                _permits--;
+                return Task.CompletedTask;
+            }
+            else
+            {
+                var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+                _waiters.Enqueue(tcs);
+                return tcs.Task;
+            }
+        }
+    }
+
+    public void Set(int count = 1)
+    {
+        lock (_lock)
+        {
+            while (count > 0 && _waiters.Count > 0)
+            {
+                var tcs = _waiters.Dequeue();
+                tcs.TrySetResult(true);
+                count--;
+            }
+
+            // 如果没有等待者，就存到配额里
+            _permits += count;
+        }
+    }
+}

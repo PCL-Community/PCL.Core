33727b6d76a466d95d87f82da73844a85ad0845d
pynickle <2330458484@qq.com>
2025-09-05 03:41:54 +0800

    ci: 测试

--- Minecraft/ResourceProject/Curseforge/CurseforgeScreenshots.cs
+++ Minecraft/ResourceProject/Curseforge/CurseforgeScreenshots.cs
@@ -1,11 +1,11 @@
-﻿using System;
-
-namespace PCL.Core.Model.ResourceProject.Curseforge;
-
-[Serializable]
-public record CurseforgeScreenshots(
-    int id,
-    int modId,
-    string title,
-    string description,
-    string thumbnailUrl);
+﻿using System;//
+//
+namespace PCL.Core.Model.ResourceProject.Curseforge; //
+//
+[Serializable]//
+public record CurseforgeScreenshots(//
+    int id,//
+    int modId,//
+    string title,//
+    string description,//
+    string thumbnailUrl);//

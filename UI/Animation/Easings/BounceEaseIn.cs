d1779563f55065232f6997ff32146a25aa34161a
Hill23333 <a18035135032@163.com>
2025-09-04 19:04:19 +0800

    feat(animation): 指数衰减的反弹缓动

---
+++ UI/Animation/Easings/BounceEaseIn.cs
@@ -0,0 +1,11 @@
+﻿using PCL.Core.Utils;
+
+namespace PCL.Core.UI.Animation.Easings;
+
+public class BounceEaseIn : Easing
+{
+    protected override double EaseCore(double progress)
+    {
+        return 1 - BounceEaseUtils.Bounce(1 - progress);
+    }
+}

10ffa7e949128a5a04b30f1297354e099c0a203b
薄奚梦灵 <boximengling@outlook.com>
2025-09-09 18:18:02 +0800

    feat(secret):将 secret 迁移到 Core

---
+++ App/Secrets.cs
@@ -0,0 +1,16 @@
+using PCL.Core.Utils.Exts;
+using PCL.Core.Utils.OS;
+
+namespace PCL.Core.App;
+
+public static class Secrets
+{
+    
+    public static string MSOAuthClientId = EnvironmentInterop.GetSecret("MS_CLIENT_ID", readEnvDebugOnly: true).ReplaceNullOrEmpty();
+    public static string CurseForgeAPIKey = EnvironmentInterop.GetSecret("CURSEFORGE_API_KEY", readEnvDebugOnly: true).ReplaceNullOrEmpty();
+    public static string TelemetryKey = EnvironmentInterop.GetSecret("TELEMETRY_KEY", readEnvDebugOnly: true).ReplaceNullOrEmpty();
+    public static string NatayarkClientId = EnvironmentInterop.GetSecret("NAID_CLIENT_ID", readEnvDebugOnly: true).ReplaceNullOrEmpty();
+    public static string NatayarkClientSecret = EnvironmentInterop.GetSecret("NAID_CLIENT_SECRET", readEnvDebugOnly: true).ReplaceNullOrEmpty();
+    public static string LinkServers = EnvironmentInterop.GetSecret("LINK_SERVER_ROOT", readEnvDebugOnly: true).ReplaceNullOrEmpty();
+    public static string TurstedCertificates = EnvironmentInterop.GetSecret("LINK_SERVER_ROOT", readEnvDebugOnly: true).ReplaceNullOrEmpty();
+}

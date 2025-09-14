4030334009f39e45149f9dcfb500199b229a8bec
tangge233 <tangge233@qq.com>
2025-09-14 16:55:50 +0800

    feat(encryption): 提高 rawid 的信息熵

--- Utils/Secret/Identify.cs
+++ Utils/Secret/Identify.cs
@@ -14,41 +14,38 @@ public class Identify
     public static readonly Lazy<SecureString> EncryptionKey = new(_getEncryptionKey);
     public static readonly Lazy<string> LauncherId = new(_getLauncherId);
 
-    private const string DefaultRawId = "c34f48114e31f6bb99bdf720f0d98a3c74e325a90c3a8e638d3687a83d404b0c3687b6c0b7a752d718dd5e27a6be6ab68a7f8aa996de158052190bf3ffd17c61";
-
     private static SecureString _getRawId()
     {
-        string? code = null;
+        var code = new StringBuilder();
         try
         {
-            using var searcher = new ManagementObjectSearcher("SELECT UUID FROM Win32_ComputerSystemProduct");
-            using var results = searcher.Get();
-            foreach (var managementObject in results)
-            {
-                var uuid = managementObject?["UUID"];
-                if (uuid is null) continue;
-                code = uuid.ToString();
-                break;
-            }
-        }
-        catch (ManagementException ex)
-        {
-            LogWrapper.Error(ex, "Identify", $"WMI 查询失败，请检查组件是否正常");
-        }
-        catch (System.Runtime.InteropServices.COMException ex)
-        {
-            LogWrapper.Error(ex, "Identify", $"COM 异常，请确保WMI服务正在运行");
-        }
-        catch (UnauthorizedAccessException ex)
-        {
-            LogWrapper.Error(ex, "Identify", "访问被异常拒绝，请尝试以管理员权限运行");
+            code.Append(_GetWmiProperty("Win32_ComputerSystemProduct", "UUID"))
+                .Append(_GetWmiProperty("Win32_BaseBoard", "Product"))
+                .Append(_GetWmiProperty("Win32_BaseBoard", "SerialNumber"));
         }
         catch (Exception ex)
         {
-            LogWrapper.Error(ex, "Identify", $"意外的系统异常");
+            LogWrapper.Error(ex, "Identify", "获取设备基础信息失败");
         }
 
-        return (code is null ? DefaultRawId : SHA512Provider.Instance.ComputeHash(code)).ToSecureString();
+        return SHA512Provider.Instance.ComputeHash(code.ToString()).ToSecureString();
+    }
+
+    private static string _GetWmiProperty(string className, string propertyName)
+    {
+        try
+        {
+            using var searcher =
+                new ManagementObjectSearcher($"SELECT {propertyName} FROM {className}");
+            using var results = searcher.Get();
+            foreach (var obj in results)
+            {
+                if (obj[propertyName] is not null)
+                    return (obj[propertyName].ToString() ?? string.Empty).Trim();
+            }
+        }
+        catch { /* Ignore */ }
+        return string.Empty;
     }
 
     private static SecureString _getEncryptionKey()

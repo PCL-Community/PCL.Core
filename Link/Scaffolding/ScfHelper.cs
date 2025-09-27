5a52808c27eead49005729e91b5e3ba16277c756
WT2024 <1246183113@qq.com>
2025-09-27 17:34:54 +0800

    feat: 实现服务的基本架构

---
+++ Link/Scaffolding/ScfHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using PCL.Core.Logging;
+using PCL.Core.Net;
+
+namespace PCL.Core.Link.Scaffolding;
+
+public class ScfHelper : ILinkHelper
+{
+    public string Name => "Scaffolding";
+    public Socket? Socket { get; set; }
+    public CancellationTokenSource _ctx { get; set; }
+
+    public int Launch()
+    {
+        throw new System.NotImplementedException();
+    }
+    public int Close()
+    {
+        try
+        {
+            Socket?.SafeClose();
+            _ctx?.Cancel();
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            return 1;
+        }
+    }
+
+    ~ScfHelper()
+    {
+        Dispose(false);
+    }
+    
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+    
+    private void Dispose(bool disposing)
+    {
+        if (!disposing) return;
+        GC.SuppressFinalize(this);
+        Close();
+    }
+}

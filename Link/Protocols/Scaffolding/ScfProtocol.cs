41f4cd1039c7bd2f2c40812ba2ac739b8f7b2f26
吴桐 <162002839+wtommy932@users.noreply.github.com>
2025-09-28 16:44:28 +0800

    feat: 写了一点点客户端逻辑(总算开工了)

--- Link/Protocols/Scaffolding/ScfProtocol.cs
+++ Link/Protocols/Scaffolding/ScfProtocol.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading;
 using System.Threading.Tasks;
+using PCL.Core.Link.Protocols.Scaffolding.Packet;
 using PCL.Core.Net;
 using PCL.Core.Utils.Secret;
 
@@ -28,13 +32,50 @@ public class ScfProtocol(bool isServer) : LinkProtocol(isServer)
     protected override async Task ClientPart(CancellationToken token)
     {
         _ = Task.Run(() => ClientLoop(token), token);
+        var packet = new ClientPacket
+        {
+            PacketType = "c:protocols",
+            Body = Encoding.UTF8.GetBytes(string.Join("\0", _supportedProtocols))
+        };
+        var response = await _tcpHelper.SendToServer(packet.To()); // TODO: 处理响应数据
+        
+        packet = new ClientPacket
+        {
+            PacketType = "c:server_port",
+            Body = []
+        };
+        response = await _tcpHelper.SendToServer(packet.To()); // TODO: 处理响应数据
     }
     
     private async Task ClientLoop(CancellationToken token)
     {
         while (!token.IsCancellationRequested)
         {
-            // TODO 等待ClientPacket类写好
+            var packet = new ClientPacket
+            {
+                PacketType = "c:player_ping",
+                Body = JsonObjectToBytes(new JsonObject
+                {
+                    ["name"] = "", // TODO: 添加玩家信息
+                    ["machine_id"] = "", // TODO: 添加玩家ID
+                    ["vendor"] = "PCL2-CE"
+                })
+            };
+            await _tcpHelper.SendToServer(packet.To(), false);
+            packet = new ClientPacket()
+            {
+                PacketType = "c:player_profile_list",
+                Body = []
+            };
+            var response = await _tcpHelper.SendToServer(packet.To()); // TODO: 处理响应数据
+
+            await Task.Delay(5000);
         }
+    }
+    
+    private byte[] JsonObjectToBytes(JsonObject obj)
+    {
+        var jsonString = obj.ToJsonString();
+        return System.Text.Encoding.UTF8.GetBytes(jsonString);
     }
 }

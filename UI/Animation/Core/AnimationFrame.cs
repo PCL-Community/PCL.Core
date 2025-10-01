ffaf1a27f471b162d0935d849144928b33db6c56
Hill23333 <a18035135032@163.com>
2025-10-02 00:10:06 +0800

    feat: 动画核心

---
+++ UI/Animation/Core/AnimationFrame.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+using PCL.Core.UI.Animation.Animatable;
+
+namespace PCL.Core.UI.Animation.Core;
+
+public readonly struct AnimationFrame<T> : IAnimationFrame where T : IAdditionOperators<T, T, T>
+{
+    public IAnimatable Target { get; init; }
+    public T Value { get; init; }
+    public T StartValue { get; init; }
+    public T GetAbsoluteValue() => StartValue + Value;
+    object IAnimationFrame.GetAbsoluteValue() => GetAbsoluteValue();
+}

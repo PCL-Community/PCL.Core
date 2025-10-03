11d464a71dc732a53789a338d295b2bbf5899467
薄奚梦灵 <boximengling@outlook.com>
2025-10-03 22:22:29 +0800

    Update App/Secrets.cs
    Co-authored-by: tangge233 <50769997+tangge233@users.noreply.github.com>

--- App/Secrets.cs
+++ App/Secrets.cs
@@ -11,7 +11,7 @@ public static class Secrets
     public static string TelemetryKey = EnvironmentInterop.GetSecret("TELEMETRY_KEY", readEnvDebugOnly: true).ReplaceNullOrEmpty();
     public static string NatayarkClientId = EnvironmentInterop.GetSecret("NAID_CLIENT_ID", readEnvDebugOnly: true).ReplaceNullOrEmpty();
     public static string NatayarkClientSecret = EnvironmentInterop.GetSecret("NAID_CLIENT_SECRET", readEnvDebugOnly: true).ReplaceNullOrEmpty();
-    public static string LinkServers = EnvironmentInterop.GetSecret("LINK_SERVER_ROOT", readEnvDebugOnly: true).ReplaceNullOrEmpty();
+    public static string[] LinkServers = EnvironmentInterop.GetSecret("LINK_SERVER_ROOT", readEnvDebugOnly: true).ReplaceNullOrEmpty().Split("|");
     public static string CommitHash = EnvironmentInterop.GetSecret("GITHUB_SHA", readEnvDebugOnly: true).ReplaceNullOrEmpty();
     public static string[] TurstedCertificates = EnvironmentInterop.GetSecret("LINK_SERVER_ROOT", readEnvDebugOnly: true).ReplaceNullOrEmpty().Split("|");
 }

99c7659901e3a60bdfcb16894851bac88e4ec1c5
吴桐 <162002839+wtommy932@users.noreply.github.com>
2025-10-05 22:48:54 +0800

    chore: 写一个架构

---
+++ Link/Parser/IParser.cs
@@ -0,0 +1,13 @@
+﻿using static PCL.Core.Link.Lobby.LobbyInfoProvider;
+
+namespace PCL.Core.Link.Parser;
+
+/// <summary>
+/// 管理器，用于解析和生成lobby信息
+/// </summary>
+public interface IParser
+{
+    protected bool Validate(string code);
+    public LobbyInfo Parse(string code);
+    public string Generate(LobbyInfo info);
+}

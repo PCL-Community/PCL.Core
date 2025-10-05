d2371bf532e6c85255240529f7b3f1c9c8ad2abb
吴桐 <162002839+wtommy932@users.noreply.github.com>
2025-10-05 22:48:54 +0800

    chore: 写一个架构

---
+++ Link/Parser/CEParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static PCL.Core.Link.Lobby.LobbyInfoProvider;
+
+namespace PCL.Core.Link.Parser;
+
+public class CEParser : IParser
+{
+    public bool Validate(string code)
+    {
+        throw new NotImplementedException();
+    }
+
+    public LobbyInfo Parse(string code)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Generate(LobbyInfo info)
+    {
+        throw new NotImplementedException();
+    }
+}

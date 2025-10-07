7c96d9f34c2f7783b7c3f46d69e1c3d57afbfc2b
吴桐 <162002839+wtommy932@users.noreply.github.com>
2025-10-07 11:09:52 +0800

    ref: 改变文件结构

--- Link/Lobby/Parser/IParser.cs
+++ Link/Lobby/Parser/IParser.cs
@@ -1,6 +1,6 @@
 ﻿using static PCL.Core.Link.Lobby.LobbyInfoProvider;
 
-namespace PCL.Core.Link.Lobby.LobbyId;
+namespace PCL.Core.Link.Lobby.Parser;
 
 /// <summary>
 /// LobbyId解析器接口，用于解析各个启动器生成的LobbyId

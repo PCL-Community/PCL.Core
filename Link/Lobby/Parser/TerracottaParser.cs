336c3336d77b45aeab7d701ee9aa2693381cacd6
吴桐 <162002839+wtommy932@users.noreply.github.com>
2025-10-07 11:32:33 +0800

    chore: 添加陶瓦类的架构

---
+++ Link/Lobby/Parser/TerracottaParser.cs
@@ -0,0 +1,16 @@
+﻿using static PCL.Core.Link.Lobby.LobbyInfoProvider;
+
+namespace PCL.Core.Link.Lobby.Parser;
+
+public class TerracottaParser : IParser
+{
+    public bool Validate(string code)
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    public LobbyInfo Parse(string code)
+    {
+        throw new System.NotImplementedException();
+    }
+}

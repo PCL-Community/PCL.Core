100cddcc10fc1a75665ac072cef9e5b697cc162b
Hill23333 <104297546+Hill23333@users.noreply.github.com>
2025-10-18 12:34:57 +0800

    Revert "feat(animation): 新动画系统 (#69)"
    This reverts commit 46bab54cb293e7ad343b8de922868b957d532254.

--- AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Windows.Markup;
-
-[assembly: XmlnsDefinition("https://ce.pclc.cc/core/ui/animation", "PCL.Core.UI.Animation")]
-[assembly: XmlnsDefinition("https://ce.pclc.cc/core/ui/animation", "PCL.Core.UI.Animation.Core")]
-[assembly: XmlnsDefinition("https://ce.pclc.cc/core/ui/animation", "PCL.Core.UI.Animation.Easings")]
-[assembly: XmlnsPrefix("https://ce.pclc.cc/core/ui/animation", "ani")]

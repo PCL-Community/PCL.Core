7ed7bf72ecd12095facb1dc93b2c45f3a03dc3a4
WhiteCAT <64885812+whitecat346@users.noreply.github.com>
2025-10-19 23:55:31 +0800

    fix(scaf): remove some duplicated files

--- Link/Scaffolding/Abstractions/ScaffoldingResponse.cs
+++
@@ -1,5 +0,0 @@
-using System;
-
-namespace PCL.Core.Link.Scaffolding.Abstractions;
-
-public record ScaffoldingResponse(byte Status, ReadOnlyMemory<byte> Body);
--- Link/Scaffolding/Models/RoomType.cs
+++
@@ -1,8 +0,0 @@
-namespace PCL.Core.Link.Scaffolding.Models;
-
-public enum RoomType
-{
-    // ReSharper disable once InconsistentNaming
-    PCLCE,
-    Terracotta
-}

c9424a8e13f2f6df5b93ea3199e07c3d229656d2
xrlzu <xrlzu@outlook.com>
2025-11-09 15:02:52 +0800

    feat:完全移除投票页面 (#129)

--- App/Config.cs
+++ App/Config.cs
@@ -681,7 +681,6 @@ public static partial class Config
             [ConfigItem<bool>("UiHiddenSetupSystem", false, ConfigSource.Local)] public partial bool SetupSystem { get; set; }
             [ConfigItem<bool>("UiHiddenOtherHelp", false, ConfigSource.Local)] public partial bool OtherHelp { get; set; }
             [ConfigItem<bool>("UiHiddenOtherFeedback", false, ConfigSource.Local)] public partial bool OtherFeedback { get; set; }
-            [ConfigItem<bool>("UiHiddenOtherVote", false, ConfigSource.Local)] public partial bool OtherVote { get; set; }
             [ConfigItem<bool>("UiHiddenOtherAbout", false, ConfigSource.Local)] public partial bool OtherAbout { get; set; }
             [ConfigItem<bool>("UiHiddenOtherTest", false, ConfigSource.Local)] public partial bool OtherTest { get; set; }
             [ConfigItem<bool>("UiHiddenVersionEdit", false, ConfigSource.Local)] public partial bool InstanceEdit { get; set; }

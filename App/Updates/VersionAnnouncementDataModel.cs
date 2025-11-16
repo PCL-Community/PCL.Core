56a4fd0f9e9ca2e2373375d5fbdff7c350671063
吴桐 <162002839+wtommy932@users.noreply.github.com>
2025-11-16 14:32:17 +0800

    ref: 移动文件夹, 并添加 IUpdateSource 接口

---
+++ App/Updates/VersionAnnouncementDataModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PCL.Core.App.Updates;
+
+public class VersionAnnouncementDataModel
+{
+    public required List<VersionAnnouncementContentModel> Contents { get; set; }
+}
+
+public class VersionAnnouncementContentModel
+{
+    public required string Title { get; set; }
+    public required string Detail { get; set; }
+    public required string Id { get; set; }
+    public required string Date { get; set; }
+    public required AnnouncementBtnInfoModel Btn1 { get; set; }
+    public required AnnouncementBtnInfoModel Btn2 { get; set; }
+}
+
+public class AnnouncementBtnInfoModel
+{
+    public required string Text { get; set; }
+    public required string Command { get; set; }
+    public required string CommandParameter { get; set; }
+}

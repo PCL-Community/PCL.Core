3e52e40ac0e793e5999e21e14400a426f1536ab3
吴桐 <162002839+wtommy932@users.noreply.github.com>
2025-12-07 14:29:17 +0800

    feat: 基本架构, 并将 UpdateService 重构到 UpdateHandler

---
+++ App/Arguments/ArgumentHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PCL.Core.App.Arguments;
+
+public interface IArgumentHandler
+{
+    public string Identifier { get; }
+    
+    protected LifecycleContext ParentContext { get; init; }
+    
+    public HandleResult Handle(string[] args);
+}
+
+public abstract class GeneralHandler(string identifier) : IArgumentHandler
+{
+    public string Identifier => identifier;
+
+    public required LifecycleContext ParentContext { get; init; }
+
+    public abstract HandleResult Handle(string[] args);
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ArgumentHandlerAttribute : Attribute;
+
+public enum HandleResult
+{
+    /// <summary>
+    /// 参数未被处理
+    /// </summary>
+    NotHandled,
+    /// <summary>
+    /// 参数已被处理
+    /// </summary>
+    Handled,
+    /// <summary>
+    /// 参数已被处理，且请求程序退出
+    /// </summary>
+    HandledAndExit,
+}
---
+++ App/Arguments/ArgumentsService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PCL.Core.App.Arguments;
+
+[LifecycleService(LifecycleState.BeforeLoading)]
+public partial class ArgumentsService : GeneralService
+{
+    #region Lifecycle
+    
+    private static LifecycleContext? _context;
+
+    private static LifecycleContext Context => _context!;
+    
+    public ArgumentsService() : base("args", "参数处理", false) { _context = ServiceContext; }
+
+    public override void Start()
+    {
+        _InitializeHandlers();
+        _HandleArguments();
+        Context.DeclareStopped();
+    }
+    #endregion
+    
+    #region Arguments Handle
+    
+    private void _HandleArguments()
+    {
+        var args = Basics.CommandLineArguments;
+
+        foreach (var handler in _handlers)
+        {
+            var result = handler.Handle(args);
+            switch (result)
+            {
+                case HandleResult.NotHandled: break;
+                case HandleResult.Handled:
+                    Context.Info($"参数已被处理器 {handler.Identifier} 处理");
+                    return;
+                case HandleResult.HandledAndExit:
+                    Context.Info($"参数已被处理器 {handler.Identifier} 处理，程序将退出");
+                    Context.RequestExit();
+                    return;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+        }
+
+        Context.Debug("无匹配的命令行参数处理器");
+    }
+    #endregion
+}

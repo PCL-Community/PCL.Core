715387353322372ba6009d6ca62f3aa0eff783f3
WhiteCAT <64885812+whitecat346@users.noreply.github.com>
2025-12-27 16:23:31 +0800

    chore: remove some file

--- App/Updates/UpdateHelper.cs
+++ App/Updates/UpdateHelper.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using PCL.Core.Logging;
+using PCL.Core.Utils;
+using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Threading;
-using PCL.Core.Logging;
+using System.Threading.Tasks;
 
 namespace PCL.Core.App.Updates;
 
@@ -15,46 +15,59 @@ public static class UpdateHelper
     /// </summary>
     /// <param name="source">用于替换的来源文件路径</param>
     /// <param name="target">目标文件路径</param>
-    public static Exception? Replace(string source, string target)
+    /// <exception cref="FileNotFoundException">Throws if backup file not found</exception>
+    public static async Task<Exception?> ReplaceAsync(string source, string target)
     {
         var backup = $"{target}.bak.{DateTime.Now:yyyyMMddHHmmss}";
         Exception? lastEx = null;
         try
         {
+            // get full file path. e.g. /example/ex.txt -> C:/example/ex.txt
             source = Path.GetFullPath(source);
             target = Path.GetFullPath(target);
+
             // 备份目标文件
             File.Copy(target, backup);
-            if (!File.Exists(backup)) throw new FileNotFoundException("备份目标文件失败", backup);
-            // 删除原文件并等待文件删除事件
-            var watcher = new FileSystemWatcher(Basics.GetParentPathOrEmpty(target), Path.GetFileName(target));
-            var deletedEvent = new ManualResetEventSlim(false);
-            watcher.Deleted += (_, _) => deletedEvent.Set();
-            watcher.EnableRaisingEvents = true;
-            File.Delete(target);
-            if (!deletedEvent.Wait(TimeSpan.FromSeconds(3))) throw new TimeoutException("删除目标文件失败");
-            watcher.EnableRaisingEvents = false;
-            watcher.Dispose();
+            if (!File.Exists(backup))
+            {
+                throw new FileNotFoundException("备份目标文件失败", backup);
+            }
+
+            // delete origin file
+            await FileDeleteHelper.DeleteFileAndWaitAsync(target).ConfigureAwait(false);
+
             // 复制到目标文件
             File.Copy(source, target);
-            if (!File.Exists(target)) throw new FileNotFoundException("复制到目标文件失败", target);
+            if (!File.Exists(target))
+            {
+                throw new FileNotFoundException("复制到目标文件失败", target);
+            }
         }
         catch (Exception ex)
         {
             // 出错：恢复原文件并返回异常
-            if (File.Exists(backup) && !File.Exists(target)) File.Move(backup, target);
+            if (File.Exists(backup) && !File.Exists(target))
+            {
+                File.Move(backup, target);
+            }
+
             lastEx = ex;
         }
-        if (File.Exists(backup)) File.Delete(backup); // 删除备份文件
+
+        if (File.Exists(backup))
+        {
+            File.Delete(backup);
+        }
+
         return lastEx;
     }
-    
+
+    // ReSharper disable once FlagArgument
     public static void Restart(bool triggerRestartAndByEnd)
     {
         try
         {
-            var fileName = Path.GetFullPath(Path.Combine(Basics.ExecutableDirectory, "PCL", 
-                "Plain Craft Launcher Community Edition.exe"));
+            var fileName = Path.GetFileName(Environment.ProcessPath);
 
             if (!File.Exists(fileName))
             {
@@ -62,20 +75,26 @@ public static class UpdateHelper
                 return;
             }
 
-            var args = $"update {Environment.ProcessId} \"{Basics.ExecutablePath}\" \"{fileName}\" true";
 
             var startInfo = new ProcessStartInfo(fileName)
             {
-                Arguments = args,
+                ArgumentList =
+                {
+                    "update",
+                    Environment.ProcessId.ToString(),
+                    $"{Basics.ExecutablePath}",
+                    $"{fileName}",
+                    "true"
+                },
                 WindowStyle = ProcessWindowStyle.Hidden,
                 CreateNoWindow = true
             };
 
             Process.Start(startInfo);
-            LogWrapper.Info("Update", "已启动更新程序,参数: " + args);
+            LogWrapper.Info("Update", "已尝试启动更新程序,参数: " + startInfo.ArgumentList);
 
             if (!triggerRestartAndByEnd) return;
-            
+
             LogWrapper.Info("Update", "已由于更新强制结束程序");
             Environment.Exit(0);
         }
@@ -88,10 +107,4 @@ public static class UpdateHelper
             // 忽略其他启动异常以保持方法简洁（可按需记录）
         }
     }
-}
-
-public enum UpdateChannel
-{
-    Stable,
-    Beta
 }

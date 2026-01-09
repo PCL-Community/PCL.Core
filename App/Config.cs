ca0c64c3d50c43e7af9a800221e827b4c2bad408
xrlzu <xrlzu@outlook.com>
2026-01-09 18:18:00 +0800

    改个字

--- App/Config.cs
+++ App/Config.cs
@@ -450,7 +450,7 @@ public static partial class Config
         [ConfigItem<bool>("UiLockWindowSize", false)] public partial bool LockWindowSize { get; set; }
 
         /// <summary>
-        /// 在启动时显示“你知道吗”提示。
+        /// 在启动游戏时显示你知道吗。
         /// </summary>
         [ConfigItem<bool>("UiShowLaunchingHint", true, ConfigSource.Local)] public partial bool ShowLaunchingHint { get; set; }
 
